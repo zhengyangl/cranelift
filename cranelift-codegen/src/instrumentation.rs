b6baada6614795c540a8d6be6ff74cb9ff53d0b3
Zhengyang Liu <liuz@cs.utah.edu>
2019-12-02 02:08:45 +0000

    foo

--- cranelift-codegen/src/instrumentation.rs
+++ cranelift-codegen/src/instrumentation.rs
@@ -5,14 +5,14 @@ use crate::ir::{
     immediates,
     instructions::{Opcode, ValueList},
     types::{I16, I32, I64, I8},
+    entities::Value,
     DataFlowGraph, Ebb, Function, Inst, InstBuilder, InstructionData, Type, LibCall, ExternalName, Signature, ExtFuncData, AbiParam
 };
 use crate::isa::{CallConv, TargetIsa};
 use crate::flowgraph::ControlFlowGraph;
-use alloc::vec::Vec;
 
 /// The main pre-opt pass.
-pub fn do_instrumentation(func: &mut Function, cfg: &mut ControlFlowGraph, isa: &dyn TargetIsa) {
+pub fn do_instrumentation(func: &mut Function, _cfg: &mut ControlFlowGraph, isa: &dyn TargetIsa) {
     print!("{}\n",func);
 
     // look at only user defined functions
@@ -30,16 +30,49 @@ pub fn do_instrumentation(func: &mut Function, cfg: &mut ControlFlowGraph, isa: 
             colocated: isa.flags().colocated_libcalls(),
         });
 
+        let funchook_enter = func.import_function(ExtFuncData {
+            name: ExternalName::LibCall(LibCall::FuncHookEnter),
+            signature: sigref,
+            colocated: isa.flags().colocated_libcalls(),
+        });
+
+        let funchook_exit = func.import_function(ExtFuncData {
+            name: ExternalName::LibCall(LibCall::FuncHookExit),
+            signature: sigref,
+            colocated: isa.flags().colocated_libcalls(),
+        });
+
         let mut pos = FuncCursor::new(func);
+
+        let mut is_entry: bool = true;
+        let mut function_i: Option<Value> = None;
+        let mut function_ns: Option<Value> = None;
 
         // For each instruction in each Basic block
         while let Some(_ebb) = pos.next_ebb() {
             while let Some(_inst) = pos.next_inst()
             {
-                let function_ns = pos.ins().iconst(I32, namespace as i64);
-                let function_i = pos.ins().iconst(I32, index as i64);
+                let is_return = pos.func.dfg[_inst].opcode().is_return();
+                if is_entry {
+                    let f_i = pos.ins().iconst(I32, namespace as i64);
+                    let f_ns = pos.ins().iconst(I32, index as i64);
+                    function_i = Some(f_i);
+                    function_ns = Some(f_ns);
 
-                pos.ins().call(insthook, &[function_ns, function_i]);
+                    pos.ins().call(funchook_enter, &[f_ns, f_i]);
+                    is_entry = false;
+                }
+
+
+                if let (Some(f_i), Some(f_ns)) = (function_i, function_ns) {
+                    pos.ins().call(insthook, &[f_ns, f_i]);
+                    if is_return {
+                        pos.ins().call(funchook_exit, &[f_ns, f_i]);
+                    }
+                } else {
+                    panic! ("Error: function info has not been inserted as constants");
+                }
+
             }
         }
 
